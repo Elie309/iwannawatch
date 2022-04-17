3179a6907e0840f9f69204cbcec8d4a524fe6b2a
elie309 <elie.saade432@gmail.com>
2022-04-17 11:32:09 +0300

    First upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+
+const App: React.FC = () => {
+
+
+  return (
+    <div className="App">
+      <ResponsiveAppBar />
+    </div>
+  );
+}
+
+export default App;
